32001d7a1f6099af473acbeb39b8a9e49cf87307
fukiame <fukiame@proton.me>
2023-11-28 18:09:55 +0700

    clm

--- novivo/nuke.sh
+++ novivo/nuke.sh
@@ -5,7 +5,7 @@ do
   elif [ "$l" = "" ]; then :
   else
     echo "  removing $l"
-    #pm uninstall --user 0 "$l" 1> /dev/null
-    #pm clear "$l" 1> /dev/null
+    pm uninstall --user 0 "$l" 1> /dev/null
+    pm clear "$l" 1> /dev/null
   fi
 done < "$(pwd)/target.txt"
