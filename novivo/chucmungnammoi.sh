606663d1f17b48643d89b0cfee0c68bfb56e3d3b
fukiame <fukiame@proton.me>
2023-11-28 10:05:52 +0700

    3

--- novivo/chucmungnammoi.sh
+++ novivo/chucmungnammoi.sh
@@ -1,7 +1,8 @@
 while read -r l
 do
-  if [ "$l" = *"#"* ]; then echo "$l"
+  if [ "$l" = "#"* ]; then echo "$l"
   else if [ "$l" = "" ]; then :
+  else if [ "$l" = ":"* ]; then echo "skipping $l"
   else
     pm uninstall --user 0 "$line"
     pm clear "$line"
