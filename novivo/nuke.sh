07927a30e40ee10f406621982bd3a3a40e9b3ec3
fukiame <fukiame@proton.me>
2023-11-28 17:52:53 +0700

    fix

--- novivo/nuke.sh
+++ novivo/nuke.sh
@@ -1,11 +1,11 @@
 while read -r l
 do
   if [ "$l" = "#"* ]; then echo "$l"
-  else if [ "$l" = ":"* ]; then echo "  skipping $l"
-  else if [ "$l" = "" ]; then :
+  elif [ "$l" = ":"* ]; then echo "  skipping $l"
+  elif [ "$l" = "" ]; then :
   else
     echo "  removing $l"
-    pm uninstall --user 0 "$l" 1> /dev/null
-    pm clear "$l" 1> /dev/null
+    #pm uninstall --user 0 "$l" 1> /dev/null
+    #pm clear "$l" 1> /dev/null
   fi
 done < "$(pwd)/target.txt"
